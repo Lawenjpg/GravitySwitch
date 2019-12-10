d33cff7f73a2bf178dc7c6328e44c9c6e7a449c9
Cody Santana <CodyLawrence2014@gmail.com>
2019-12-10 15:17:38 -0500

    Updated number 1

---
+++ Assets/Scripts/DeathScrip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScrip : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+    public float forceAmt = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+  
+    void OnCollisionEnter(Collision colReport)
+    {
+        if (colReport.gameObject.CompareTag("collectible"))
+        {
+            Destroy(colReport.gameObject);
+        }
+    }
+}
