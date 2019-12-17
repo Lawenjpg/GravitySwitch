b68ad73cbf007b0ae3dcd411c7b54760a1d34b69
Donrock123 <47193614+Donrock123@users.noreply.github.com>
2019-12-16 22:30:28 -0500

    Added Death Scrips and level 4

---
+++ Assets/Scripts/DestroyedByBoundary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedByBoundary : MonoBehaviour
+{
+    private void Start()
+    {
+      GetComponent<Rigidbody>(); 
+    }
+    void OnTriggerExit(Collider colReport)
+    {
+        if (colReport.gameObject.CompareTag("Player"))
+        {
+            Destroy(colReport.gameObject);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   //rivate int hitpoint = 1;
+   //rivate int score = 0;
+
+    public Transform spawnPosition;
+    public Transform playerTransform;
+
+    //Every single fram
+    private void Update()
+    {
+        if (playerTransform.position.y < -10)
+        {
+            playerTransform.position = spawnPosition.position;
+            //itpoint--; //hitpoint = hitpoint - 1;
+            //if(hitpoint <= 0)
+           
+            {
+                Debug.Log("Failure");
+            }
+        }
+    }
+}
